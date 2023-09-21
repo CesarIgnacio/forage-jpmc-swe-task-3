166d3d1de749d2e7d80a04716dc5130869936254
CesarIgnacio <cesarignac@gmail.com>
2023-09-21 14:36:19 -0400

    update

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -15,8 +15,8 @@ export class DataManipulator {
     const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
     const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
     const ratio = priceABC / priceDEF;
-    const upperBound = 10 + 0.05;
-    const lowerBound = 10 - 0.05;
+    const upperBound = 1 + 0.05;
+    const lowerBound = 1 - 0.05;
     return {
       price_abc: priceABC,
       price_def: priceDEF,
